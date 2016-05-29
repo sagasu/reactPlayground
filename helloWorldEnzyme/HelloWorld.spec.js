2f2c815718b8e1b7714229901c7f06b9dc21e6a3
sagasu <sagasu@gmail.com>
2016-05-29 22:53:15 +0100

    adding additional test to check enzyme power.

--- helloWorldEnzyme/HelloWorld.spec.js
+++ helloWorldEnzyme/HelloWorld.spec.js
@@ -9,5 +9,7 @@ describe('HelloWorld component', () => {
         // How much simpler is that
         const wrapper = shallow(<HelloWorld/>)
         expect(wrapper.equals(<div>Hello world</div>)).to.be.true
+        expect(wrapper.find('div')).to.have.length(1)
+        expect(wrapper.contains('Hello world')).to.be.true
     })
 })
